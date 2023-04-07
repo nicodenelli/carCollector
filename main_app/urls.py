4821e07d9e3e83687e9092f0f12b057639062002
nicodenelliGH <ndm9412@gmail.com>
2023-04-07 16:18:28 -0400

    adding cars index route

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    # route for cars index
+    path('cars/', views.cars_index, name='index'),
 ]
