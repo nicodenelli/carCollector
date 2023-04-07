aa015fcdaf8461ebd5405f4790525e73276788ba
nicodenelliGH <ndm9412@gmail.com>
2023-04-07 16:27:06 -0400

    creating cars views model

--- main_app/views.py
+++ main_app/views.py
@@ -6,6 +6,22 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
+class Car:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, year, make, model, color):
+    self.year = year
+    self.make = make
+    self.model = model
+    self.color = color
+
+cars = [ # this cars points to below where {'cars': cars}... the blue one
+  Car(1969, 'Camaro', 'R/S Zl1', 'racing yellow'),
+  Car('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Car('Raven', 'black tripod', '3 legged cat', 4)
+]
+
+def cars_index(request):
+  return render(request, 'cars/index.html', { 'cars': cars })# <- the blue one
+
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
