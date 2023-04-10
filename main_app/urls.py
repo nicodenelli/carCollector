9efbd0234391c0a51e9cad6c19e621ec758a8caf
nicodenelliGH <ndm9412@gmail.com>
2023-04-10 12:16:41 -0400

    adding style

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    # path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     # route for cars index
     path('cars/', views.cars_index, name='index'),
--- main_app/views.py
+++ main_app/views.py
@@ -6,25 +6,30 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
-class Car:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, make, color, model, year):
-    self.make = make
-    self.color = color
-    self.model = model
-    self.year = year
-
-cars = [ # this cars points to below where {'cars': cars}... the blue one
-  Car('Camaro', 'racing yellow', 'R/S Zl1', 1969),
-  Car('Torino', 'supersonic red', '429 Cobra Jet', 1971),
-  Car('Aston Martin', 'Silver Birch', 'DB5', 1964)
-]
+from .models import Car
 
 def cars_index(request):
+  cars = Car.objects.all() # finding all the cars from the db
   return render(request, 'cars/index.html', { 'cars': cars })# <- the blue one
 
+# class Car:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, make, color, model, year):
+#     self.make = make
+#     self.color = color
+#     self.model = model
+#     self.year = year
+
+# cars = [ # this cars points to below where {'cars': cars}... the blue one
+#   Car('Camaro', 'racing yellow', 'R/S Zl1', 1969),
+#   Car('Torino', 'supersonic red', '429 Cobra Jet', 1971),
+#   Car('Aston Martin', 'Silver Birch', 'DB5', 1964)
+# ]
+
+
+
 # Define the home view
-def home(request):
-  return render(request, 'cars/index.html', { 'cars': cars })
+# def home(request):
+#   return render(request, 'cars/index.html', { 'cars': car })
 #   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\</h1>')
 
 def about(request):
